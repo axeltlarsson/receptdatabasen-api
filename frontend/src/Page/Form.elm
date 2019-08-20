692b0e793618446517ed879ed199920babd39638
AxelTLarsson <mail@axellarsson.nu>
2019-08-20 21:55:00 +0200

    Extract form from Test into own module
    It does make the Test page much more light-weight. The idea is to now
    simply use the new Form module in the Editor module. Letting the Editor
    code handle server communication etc and all the stuff around the actual
    form. And the actual form will handle the form.
    
    One downside of this is that I must (?) use the translator pattern,
    mapping subModels and subMsgs back and forth etc in the Test page. It's
    not simply a matter of outsourcing the code just like that...

---
+++ frontend/src/Page/Form.elm
@@ -0,0 +1,359 @@
+module Page.Form exposing (Model, Msg, init, update, view)
+
+import Form exposing (Form)
+import Form.Error exposing (ErrorValue(..))
+import Form.Field as Field exposing (Field)
+import Form.Input as Input
+import Form.Validate as Validate exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (class, max, min, placeholder, value)
+import Html.Events exposing (keyCode, onClick, onInput, preventDefaultOn)
+import Json.Decode as Decode
+import Regex
+
+
+
+-- MODEL
+
+
+type alias Recipe =
+    { title : String
+    , description : Maybe String
+    , portions : Int
+    , instructions : String
+    , ingredients : List IngredientGroup
+    , newIngredientGroupInput : String
+    , tags : List String
+    , newTagInput : String
+    }
+
+
+type alias IngredientGroup =
+    { group : String
+    , ingredients : List String
+    , newIngredientInput : String
+    }
+
+
+type alias RecipeForm =
+    Form () Recipe
+
+
+type alias Model =
+    { form : RecipeForm
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { form = initialForm }, Cmd.none )
+
+
+initialForm : RecipeForm
+initialForm =
+    Form.initial
+        [ ( "portions", Field.string "4" )
+        , ( "ingredients"
+          , Field.list
+                [ Field.group
+                    [ ( "group", Field.string "Ingredienser" )
+                    , ( "newIngredientInput", Field.value Field.EmptyField )
+                    ]
+                ]
+          )
+        ]
+        validate
+
+
+validate : Validation () Recipe
+validate =
+    succeed Recipe
+        -- TODO: validate title uniqueness (async against server)
+        |> andMap (field "title" (string |> andThen (minLength 3) |> andThen (maxLength 512)))
+        -- TODO: maybe removes the error...
+        |> andMap (field "description" (maybe (string |> andThen (maxLength 500))))
+        |> andMap (field "portions" (int |> andThen (minInt 1) |> andThen (maxInt 100)))
+        -- TODO: check non-empty tags/ingredients and trimming and such
+        |> andMap (field "instructions" (string |> andThen (minLength 5) |> andThen (maxLength 4000)))
+        |> andMap (field "ingredients" (list validateIngredientGroups))
+        |> andMap (field "newIngredientGroupInput" string)
+        |> andMap (field "tags" (list string))
+        |> andMap (field "newTagInput" string)
+
+
+validateIngredientGroups : Validation () IngredientGroup
+validateIngredientGroups =
+    succeed IngredientGroup
+        |> andMap (field "group" string)
+        |> andMap (field "ingredients" (list string))
+        |> andMap (field "newIngredientInput" string)
+
+
+view : Model -> Html Msg
+view { form } =
+    Html.map FormMsg (viewForm form)
+
+
+viewForm : RecipeForm -> Html Form.Msg
+viewForm form =
+    let
+        errorFor field =
+            case field.liveError of
+                Just error ->
+                    div [ class "error" ] [ text (errorString error field.path) ]
+
+                Nothing ->
+                    text ""
+
+        title =
+            Form.getFieldAsString "title" form
+
+        description =
+            Form.getFieldAsString "description" form
+
+        portions =
+            Form.getFieldAsString "portions" form
+
+        instructions =
+            Form.getFieldAsString "instructions" form
+
+        ingredients =
+            Form.getListIndexes "ingredients" form
+
+        newIngredientGroupInput =
+            Form.getFieldAsString "newIngredientGroupInput" form
+
+        tags =
+            Form.getListIndexes "tags" form
+
+        newTagInput =
+            Form.getFieldAsString "newTagInput" form
+    in
+    div [ class "todo-list" ]
+        [ div [ class "title" ]
+            [ Input.textInput title [ placeholder "Namn på receptet..." ]
+            , errorFor title
+            ]
+        , div [ class "description" ]
+            [ Input.textArea description [ placeholder "Beskrivning..." ]
+            , errorFor description
+            ]
+        , div [ class "portions" ]
+            [ Input.baseInput "number" Field.String Form.Text portions [ min "1", max "100" ]
+            , errorFor portions
+            ]
+        , div [ class "instructions" ]
+            [ Input.textArea instructions [ placeholder "Instruktioner..." ]
+            , errorFor instructions
+            ]
+        , div [ class "ingredients" ] <|
+            List.append [ h3 [] [ text "Ingredienser" ] ]
+                (List.map
+                    (viewFormIngredientGroup form)
+                    ingredients
+                )
+        , div [ class "new-ingredient-group" ]
+            [ Input.textInput newIngredientGroupInput
+                [ placeholder "Ny ingrediensgrupp"
+                , onEnter (Form.Append "ingredients")
+                ]
+            , errorFor newIngredientGroupInput
+            ]
+        , div [ class "tags" ] <|
+            List.append [ h3 [] [ text "Taggar" ] ]
+                (List.map
+                    (viewFormTag form)
+                    tags
+                )
+        , div [ class "new-tag-input" ]
+            [ Input.textInput newTagInput
+                [ placeholder "Ny tagg"
+                , onEnter (Form.Append "tags")
+                ]
+            , errorFor newTagInput
+            ]
+        , button
+            [ class "submit"
+            , onClick Form.Submit
+            ]
+            [ text "Spara" ]
+        ]
+
+
+viewFormIngredientGroup : RecipeForm -> Int -> Html Form.Msg
+viewFormIngredientGroup form i =
+    let
+        groupIndex =
+            "ingredients." ++ String.fromInt i
+
+        ingredients =
+            Form.getListIndexes (groupIndex ++ ".ingredients") form
+    in
+    div
+        [ class "ingredient-group" ]
+        [ Input.textInput
+            (Form.getFieldAsString (groupIndex ++ ".group") form)
+            [ placeholder "Grupp" ]
+        , button
+            [ class "remove-group"
+            , onClick (Form.RemoveItem "ingredients" i)
+            ]
+            [ text "Ta bort grupp" ]
+        , div [ class "ingredients" ] (List.map (viewFormIngredients form groupIndex) ingredients)
+        , Input.textInput (Form.getFieldAsString (groupIndex ++ ".newIngredientInput") form)
+            [ class "add-ingredient"
+            , placeholder "Ny ingrediens"
+            , onEnter (Form.Append (groupIndex ++ ".ingredients"))
+            ]
+        ]
+
+
+viewFormIngredients : RecipeForm -> String -> Int -> Html Form.Msg
+viewFormIngredients form groupIndex i =
+    let
+        index =
+            groupIndex ++ ".ingredients." ++ String.fromInt i
+    in
+    div
+        [ class "ingredient" ]
+        [ Input.textInput
+            (Form.getFieldAsString index form)
+            []
+        , button
+            [ class "remove-ingredient"
+            , onClick (Form.RemoveItem (groupIndex ++ ".ingredients") i)
+            ]
+            [ text "Ta bort ingrediens" ]
+        ]
+
+
+viewFormTag : RecipeForm -> Int -> Html Form.Msg
+viewFormTag form i =
+    div
+        [ class "tag" ]
+        [ Input.textInput
+            (Form.getFieldAsString ("tags." ++ String.fromInt i) form)
+            [ onEnter (Form.Append "tags")
+            ]
+        , button
+            [ class "remove"
+            , onClick (Form.RemoveItem "tags" i)
+            ]
+            [ text "Remove" ]
+        ]
+
+
+onEnter : msg -> Attribute msg
+onEnter msg =
+    let
+        isEnter code =
+            if code == 13 then
+                Decode.succeed ( msg, True )
+
+            else
+                Decode.fail "not ENTER"
+    in
+    preventDefaultOn "keydown" (Decode.andThen isEnter keyCode)
+
+
+errorString : ErrorValue e -> String -> String
+errorString error msg =
+    case error of
+        Empty ->
+            msg ++ " får ej vara tom"
+
+        SmallerIntThan i ->
+            msg ++ " måste vara minst " ++ String.fromInt i
+
+        GreaterIntThan i ->
+            msg ++ " får vara max " ++ String.fromInt i
+
+        ShorterStringThan i ->
+            msg ++ " måste vara minst " ++ String.fromInt i
+
+        LongerStringThan i ->
+            msg ++ " måste vara minst " ++ String.fromInt i
+
+        _ ->
+            msg ++ " är ogiltig"
+
+
+
+-- UPDATE
+
+
+type Msg
+    = FormMsg Form.Msg
+
+
+appendPrefilledValue : RecipeForm -> String -> String -> String -> RecipeForm
+appendPrefilledValue form inputFieldName listName destination =
+    let
+        newValue =
+            Maybe.withDefault "" (Form.getFieldAsString inputFieldName form).value
+
+        appendedForm =
+            Form.update validate (Form.Append listName) form
+
+        index =
+            (List.length <| Form.getListIndexes listName appendedForm) - 1
+
+        inputMsg =
+            Form.Input (String.concat [ listName, ".", String.fromInt index, destination ]) Form.Text (Field.String newValue)
+
+        resetMsg =
+            Form.Input inputFieldName Form.Text (Field.String "")
+    in
+    if String.isEmpty newValue then
+        form
+
+    else
+        appendedForm |> Form.update validate inputMsg |> Form.update validate resetMsg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg ({ form } as model) =
+    case msg of
+        FormMsg (Form.Append listName) ->
+            case listName of
+                "tags" ->
+                    ( { model | form = appendPrefilledValue form "newTagInput" "tags" "" }, Cmd.none )
+
+                "ingredients" ->
+                    ( { model | form = appendPrefilledValue form "newIngredientGroupInput" "ingredients" ".group" }, Cmd.none )
+
+                nestedIngredients ->
+                    let
+                        nestedIndex =
+                            nestedIngredientIndex nestedIngredients
+                    in
+                    case nestedIndex of
+                        Just i ->
+                            let
+                                inputFieldName =
+                                    "ingredients." ++ i ++ ".newIngredientInput"
+                            in
+                            ( { model | form = appendPrefilledValue form inputFieldName nestedIngredients "" }, Cmd.none )
+
+                        _ ->
+                            ( model, Cmd.none )
+
+        FormMsg formMsg ->
+            ( { model | form = Form.update validate formMsg form }, Cmd.none )
+
+
+nestedIngredientIndex : String -> Maybe String
+nestedIngredientIndex str =
+    let
+        regex =
+            Maybe.withDefault Regex.never <| Regex.fromString "ingredients.(\\d+).ingredients"
+
+        matches =
+            List.map .submatches <| Regex.find regex str
+    in
+    case matches of
+        [ [ Just i ] ] ->
+            Just i
+
+        _ ->
+            Nothing
--- frontend/src/Page/Test.elm
+++ frontend/src/Page/Test.elm
@@ -1,15 +1,7 @@
 module Page.Test exposing (Model, Msg, init, toSession, update, view)
 
-import Form exposing (Form)
-import Form.Error exposing (ErrorValue(..))
-import Form.Field as Field exposing (Field)
-import Form.Input as Input
-import Form.Validate as Validate exposing (..)
 import Html exposing (..)
-import Html.Attributes exposing (class, max, min, placeholder, value)
-import Html.Events exposing (keyCode, onClick, onInput, preventDefaultOn)
-import Json.Decode as Decode
-import Regex
+import Page.Form as Form
 import Session exposing (Session)
 
 
@@ -17,74 +9,29 @@ import Session exposing (Session)
 -- MODEL
 
 
-type alias RecipeForm =
-    { title : String
-    , description : Maybe String
-    , portions : Int
-    , instructions : String
-    , ingredients : List IngredientGroup
-    , newIngredientGroupInput : String
-    , tags : List String
-    , newTagInput : String
-    }
-
-
-type alias IngredientGroup =
-    { group : String
-    , ingredients : List String
-    , newIngredientInput : String
-    }
-
-
 type alias Model =
     { session : Session
-    , form : Form () RecipeForm
+    , form : Form.Model
     }
 
 
-initialForm : Form () RecipeForm
-initialForm =
-    Form.initial
-        [ ( "portions", Field.string "4" )
-        , ( "ingredients"
-          , Field.list
-                [ Field.group
-                    [ ( "group", Field.string "Ingredienser" )
-                    , ( "newIngredientInput", Field.value Field.EmptyField )
-                    ]
-                ]
-          )
-        ]
-        validate
+formToModel : Session -> Form.Model -> Model
+formToModel session form =
+    { session = session
+    , form = form
+    }
 
 
 init : Session -> ( Model, Cmd Msg )
 init session =
-    ( { session = session, form = initialForm }, Cmd.none )
+    Form.init |> updateWith (formToModel session) FormMsg
 
 
-validate : Validation () RecipeForm
-validate =
-    succeed RecipeForm
-        -- TODO: validate title uniqueness (async against server)
-        |> andMap (field "title" (string |> andThen (minLength 3) |> andThen (maxLength 512)))
-        -- TODO: maybe removes the error...
-        |> andMap (field "description" (maybe (string |> andThen (maxLength 500))))
-        |> andMap (field "portions" (int |> andThen (minInt 1) |> andThen (maxInt 100)))
-        -- TODO: check non-empty tags/ingredients and trimming and such
-        |> andMap (field "instructions" (string |> andThen (minLength 5) |> andThen (maxLength 4000)))
-        |> andMap (field "ingredients" (list validateIngredientGroups))
-        |> andMap (field "newIngredientGroupInput" string)
-        |> andMap (field "tags" (list string))
-        |> andMap (field "newTagInput" string)
-
-
-validateIngredientGroups : Validation () IngredientGroup
-validateIngredientGroups =
-    succeed IngredientGroup
-        |> andMap (field "group" string)
-        |> andMap (field "ingredients" (list string))
-        |> andMap (field "newIngredientInput" string)
+updateWith : (subModel -> Model) -> (subMsg -> Msg) -> ( subModel, Cmd subMsg ) -> ( Model, Cmd Msg )
+updateWith toModel toMsg ( subModel, subCmd ) =
+    ( toModel subModel
+    , Cmd.map toMsg subCmd
+    )
 
 
 
@@ -94,192 +41,13 @@ validateIngredientGroups =
 view : Model -> { title : String, content : Html Msg }
 view { form } =
     { title = "Test"
-    , content = div [] [ Html.map FormMsg (viewForm form) ]
+    , content = div [] [ Html.map FormMsg (Form.view form) ]
     }
 
 
-viewForm : Form () RecipeForm -> Html Form.Msg
-viewForm form =
-    let
-        errorFor field =
-            case field.liveError of
-                Just error ->
-                    div [ class "error" ] [ text (errorString error field.path) ]
-
-                Nothing ->
-                    text ""
-
-        title =
-            Form.getFieldAsString "title" form
-
-        description =
-            Form.getFieldAsString "description" form
-
-        portions =
-            Form.getFieldAsString "portions" form
-
-        instructions =
-            Form.getFieldAsString "instructions" form
-
-        ingredients =
-            Form.getListIndexes "ingredients" form
-
-        newIngredientGroupInput =
-            Form.getFieldAsString "newIngredientGroupInput" form
-
-        tags =
-            Form.getListIndexes "tags" form
-
-        newTagInput =
-            Form.getFieldAsString "newTagInput" form
-    in
-    div [ class "todo-list" ]
-        [ div [ class "title" ]
-            [ Input.textInput title [ placeholder "Namn på receptet..." ]
-            , errorFor title
-            ]
-        , div [ class "description" ]
-            [ Input.textArea description [ placeholder "Beskrivning..." ]
-            , errorFor description
-            ]
-        , div [ class "portions" ]
-            [ Input.baseInput "number" Field.String Form.Text portions [ min "1", max "100" ]
-            , errorFor portions
-            ]
-        , div [ class "instructions" ]
-            [ Input.textArea instructions [ placeholder "Instruktioner..." ]
-            , errorFor instructions
-            ]
-        , div [ class "ingredients" ] <|
-            List.append [ h3 [] [ text "Ingredienser" ] ]
-                (List.map
-                    (viewIngredientGroup form)
-                    ingredients
-                )
-        , div [ class "new-ingredient-group" ]
-            [ Input.textInput newIngredientGroupInput
-                [ placeholder "Ny ingrediensgrupp"
-                , onEnter (Form.Append "ingredients")
-                ]
-            , errorFor newIngredientGroupInput
-            ]
-        , div [ class "tags" ] <|
-            List.append [ h3 [] [ text "Taggar" ] ]
-                (List.map
-                    (viewTag form)
-                    tags
-                )
-        , div [ class "new-tag-input" ]
-            [ Input.textInput newTagInput
-                [ placeholder "Ny tagg"
-                , onEnter (Form.Append "tags")
-                ]
-            , errorFor newTagInput
-            ]
-        , button
-            [ class "submit"
-            , onClick Form.Submit
-            ]
-            [ text "Spara" ]
-        ]
-
-
-viewIngredientGroup : Form () RecipeForm -> Int -> Html Form.Msg
-viewIngredientGroup form i =
-    let
-        groupIndex =
-            "ingredients." ++ String.fromInt i
-
-        ingredients =
-            Form.getListIndexes (groupIndex ++ ".ingredients") form
-    in
-    div
-        [ class "ingredient-group" ]
-        [ Input.textInput
-            (Form.getFieldAsString (groupIndex ++ ".group") form)
-            [ placeholder "Grupp" ]
-        , button
-            [ class "remove-group"
-            , onClick (Form.RemoveItem "ingredients" i)
-            ]
-            [ text "Ta bort grupp" ]
-        , div [ class "ingredients" ] (List.map (viewIngredients form groupIndex) ingredients)
-        , Input.textInput (Form.getFieldAsString (groupIndex ++ ".newIngredientInput") form)
-            [ class "add-ingredient"
-            , placeholder "Ny ingrediens"
-            , onEnter (Form.Append (groupIndex ++ ".ingredients"))
-            ]
-        ]
-
-
-viewIngredients : Form () RecipeForm -> String -> Int -> Html Form.Msg
-viewIngredients form groupIndex i =
-    let
-        index =
-            groupIndex ++ ".ingredients." ++ String.fromInt i
-    in
-    div
-        [ class "ingredient" ]
-        [ Input.textInput
-            (Form.getFieldAsString index form)
-            []
-        , button
-            [ class "remove-ingredient"
-            , onClick (Form.RemoveItem (groupIndex ++ ".ingredients") i)
-            ]
-            [ text "Ta bort ingrediens" ]
-        ]
-
-
-viewTag : Form () RecipeForm -> Int -> Html Form.Msg
-viewTag form i =
-    div
-        [ class "tag" ]
-        [ Input.textInput
-            (Form.getFieldAsString ("tags." ++ String.fromInt i) form)
-            [ onEnter (Form.Append "tags")
-            ]
-        , button
-            [ class "remove"
-            , onClick (Form.RemoveItem "tags" i)
-            ]
-            [ text "Remove" ]
-        ]
-
-
-onEnter : msg -> Attribute msg
-onEnter msg =
-    let
-        isEnter code =
-            if code == 13 then
-                Decode.succeed ( msg, True )
-
-            else
-                Decode.fail "not ENTER"
-    in
-    preventDefaultOn "keydown" (Decode.andThen isEnter keyCode)
-
-
-errorString : ErrorValue e -> String -> String
-errorString error msg =
-    case error of
-        Empty ->
-            msg ++ " får ej vara tom"
-
-        SmallerIntThan i ->
-            msg ++ " måste vara minst " ++ String.fromInt i
-
-        GreaterIntThan i ->
-            msg ++ " får vara max " ++ String.fromInt i
-
-        ShorterStringThan i ->
-            msg ++ " måste vara minst " ++ String.fromInt i
-
-        LongerStringThan i ->
-            msg ++ " måste vara minst " ++ String.fromInt i
-
-        _ ->
-            msg ++ " är ogiltig"
+toSession : Model -> Session
+toSession { session } =
+    session
 
 
 
@@ -290,79 +58,9 @@ type Msg
     = FormMsg Form.Msg
 
 
-appendPrefilledValue : Form () RecipeForm -> String -> String -> String -> Form () RecipeForm
-appendPrefilledValue form inputFieldName listName destination =
-    let
-        newValue =
-            Maybe.withDefault "" (Form.getFieldAsString inputFieldName form).value
-
-        appendedForm =
-            Form.update validate (Form.Append listName) form
-
-        index =
-            (List.length <| Form.getListIndexes listName appendedForm) - 1
-
-        inputMsg =
-            Form.Input (String.concat [ listName, ".", String.fromInt index, destination ]) Form.Text (Field.String newValue)
-
-        resetMsg =
-            Form.Input inputFieldName Form.Text (Field.String "")
-    in
-    if String.isEmpty newValue then
-        form
-
-    else
-        appendedForm |> Form.update validate inputMsg |> Form.update validate resetMsg
-
-
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg ({ form } as model) =
+update msg { form, session } =
     case msg of
-        FormMsg (Form.Append listName) ->
-            case listName of
-                "tags" ->
-                    ( { model | form = appendPrefilledValue form "newTagInput" "tags" "" }, Cmd.none )
-
-                "ingredients" ->
-                    ( { model | form = appendPrefilledValue form "newIngredientGroupInput" "ingredients" ".group" }, Cmd.none )
-
-                nestedIngredients ->
-                    let
-                        nestedIndex =
-                            nestedIngredientIndex nestedIngredients
-                    in
-                    case nestedIndex of
-                        Just i ->
-                            let
-                                inputFieldName =
-                                    "ingredients." ++ i ++ ".newIngredientInput"
-                            in
-                            ( { model | form = appendPrefilledValue form inputFieldName nestedIngredients "" }, Cmd.none )
-
-                        _ ->
-                            ( model, Cmd.none )
-
-        FormMsg formMsg ->
-            ( { model | form = Form.update validate formMsg form }, Cmd.none )
-
-
-nestedIngredientIndex : String -> Maybe String
-nestedIngredientIndex str =
-    let
-        regex =
-            Maybe.withDefault Regex.never <| Regex.fromString "ingredients.(\\d+).ingredients"
-
-        matches =
-            List.map .submatches <| Regex.find regex str
-    in
-    case matches of
-        [ [ Just i ] ] ->
-            Just i
-
-        _ ->
-            Nothing
-
-
-toSession : Model -> Session
-toSession { session } =
-    session
+        FormMsg subMsg ->
+            Form.update subMsg form
+                |> updateWith (formToModel session) FormMsg
