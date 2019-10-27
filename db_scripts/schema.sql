8f9d8666b91e99b0ccc7fabe150f790616582d08
AxelTLarsson <mail@axellarsson.nu>
2019-10-27 21:54:17 +0100

    Refactor db model - ingredients in a jsonb col
    Abondoning the relational model to store ingredients has some benefits.
    For full text search we can have a single generated column on recipes
    that we can index, spanning both metadata on the actual recipe and
    ingredients. This allows for a more smooth implementation of search and
    it should also be faster.
    Secondly, I didn't use any strengths that the relational could have been
    used for. It was simply transformed into json when read, and input was
    transformed to the relational model before being written. This was a
    waste of CPU cycles and developer complexity.

--- db_scripts/schema.sql
+++ db_scripts/schema.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA data;
 
 --  https://stackoverflow.com/questions/31210790/indexing-an-array-for-full-text-search
-CREATE OR REPLACE FUNCTION data.immutable_array_to_string(text[]) 
+CREATE OR REPLACE FUNCTION data.immutable_array_to_string(text[])
   RETURNS text LANGUAGE sql IMMUTABLE AS 'SELECT $1::text';
 
 CREATE TABLE data.recipes(
@@ -11,16 +11,18 @@ CREATE TABLE data.recipes(
   instructions  TEXT NOT NULL CHECK (length(instructions) >= 5 AND length(instructions) <= 4000),
   tags          TEXT[] DEFAULT '{}',
   portions      INTEGER CHECK (portions > 0 and portions <= 100),
+  ingredients   JSONB,
   created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  textsearch    TSVECTOR GENERATED ALWAYS AS (
+  search        TSVECTOR GENERATED ALWAYS AS (
     to_tsvector('swedish', title || ' ' || coalesce(description, '') || ' ' || instructions)
-    || to_tsvector('swedish', immutable_array_to_string(tags)) 
+    || to_tsvector('swedish', immutable_array_to_string(tags))
+    || jsonb_to_tsvector('swedish', ingredients, '["all"]')
   ) STORED
 );
 
 CREATE INDEX recipes_title ON data.recipes (title);
-CREATE INDEX recipes_textsearch_idx ON data.recipes USING GIN (textsearch);
+CREATE INDEX recipes_search_idx ON data.recipes USING GIN (search);
 
 CREATE OR REPLACE FUNCTION set_updated_at_timestamp()
 RETURNS TRIGGER AS $$
@@ -30,26 +32,11 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- TODO: update if ingredients change - the recipes table ts should also change
 CREATE TRIGGER set_updated_at_timestamp
 BEFORE UPDATE ON data.recipes
 FOR EACH ROW
 EXECUTE PROCEDURE set_updated_at_timestamp();
 
-CREATE TABLE data.ingredient_groups(
-  id        SERIAL PRIMARY KEY,
-  recipe_id INTEGER NOT NULL REFERENCES data.recipes ON DELETE CASCADE,
-  name      TEXT NOT NULL,
-  UNIQUE(id, recipe_id, name)
-);
-CREATE INDEX ingredient_groups_recipe_id ON data.ingredient_groups (recipe_id);
-
-CREATE TABLE data.ingredients(
-  id                  SERIAL PRIMARY KEY,
-  ingredient_group_id INTEGER NOT NULL REFERENCES data.ingredient_groups ON DELETE CASCADE,
-  contents            TEXT NOT NULL
-);
-CREATE INDEX ingredients_ingredient_group_id ON data.ingredients (ingredient_group_id);
 
 CREATE SCHEMA api;
 
@@ -63,36 +50,8 @@ CREATE OR REPLACE VIEW api.recipes AS (
     portions,
     created_at,
     updated_at,
-    textsearch AS search,
-    (
-      SELECT
-        json_object_agg(name, ingredients)
-      FROM (
-        SELECT
-          name,
-          array_to_json(array_agg(ingredients.contents)) AS ingredients
-        FROM data.ingredient_groups AS ingredient_groups
-        LEFT JOIN data.ingredients AS ingredients ON ingredient_groups.id = ingredients.ingredient_group_id
-        WHERE data.recipes.id = ingredient_groups.recipe_id
-        GROUP BY recipe_id, name
-        ORDER BY recipe_id, name
-      ) groups
-    ) AS ingredients,
-
-    json_to_tsvector('swedish', (
-      SELECT
-        json_object_agg(name, ingredients)
-      FROM (
-        SELECT
-          name,
-          array_to_json(array_agg(ingredients.contents)) AS ingredients
-        FROM data.ingredient_groups AS ingredient_groups
-        LEFT JOIN data.ingredients AS ingredients ON ingredient_groups.id = ingredients.ingredient_group_id
-        WHERE data.recipes.id = ingredient_groups.recipe_id
-        GROUP BY recipe_id, name
-        ORDER BY recipe_id, name
-      ) groups
-   )::json, '["all" ]') AS ingredients_search
+    search,
+    ingredients
   FROM data.recipes
 );
 
@@ -114,30 +73,20 @@ AS $$
   DECLARE ingredient text;
 
   BEGIN
-    -- Insert the recipe
-    INSERT INTO data.recipes (title, description, instructions, tags, portions)
-            VALUES (new.title, new.description, new.instructions, new.tags, new.portions)
-              RETURNING id, created_at, updated_at INTO recipe_id, recipe_created_at, recipe_updated_at;
-
+    -- Validate ingredients json input
     IF new.ingredients IS NULL OR new.ingredients::text = '{}'::text THEN
       RAISE EXCEPTION 'A recipe must have ingredients!';
     END IF;
-    -- Insert the ingredient groups
-    FOR g, ingredients IN SELECT * FROM json_each(new.ingredients) LOOP
-      INSERT INTO data.ingredient_groups (name, recipe_id)
-        VALUES (g, recipe_id)
-        RETURNING id INTO ingredient_group_id;
-
+    FOR g, ingredients IN SELECT * FROM jsonb_each(new.ingredients) LOOP
         IF ingredients IS NULL OR json_array_length(ingredients) = 0 THEN
           RAISE EXCEPTION 'Ingredient group "%" must not be empty!', g;
         END IF;
-      -- Insert the ingredients in each ingredient group
-      FOR ingredient IN SELECT * FROM json_array_elements_text(ingredients) LOOP
-        INSERT INTO data.ingredients (contents, ingredient_group_id)
-        VALUES (ingredient, ingredient_group_id);
-      END LOOP;
-
     END LOOP;
+
+    -- Insert the recipe
+    INSERT INTO data.recipes (title, description, instructions, tags, portions, ingredients)
+           VALUES (new.title, new.description, new.instructions, new.tags, new.portions, new.ingredients)
+           RETURNING id, created_at, updated_at INTO recipe_id, recipe_created_at, recipe_updated_at;
 
     new.id = recipe_id;
     new.created_at = recipe_created_at;
@@ -158,35 +107,22 @@ AS $$
   DECLARE ingredient text;
 
   BEGIN
-    -- update the recipe
-    UPDATE data.recipes
-    SET title = new.title, description = new.description, instructions = new.instructions,
-        tags = new.tags, portions = new.portions
-    WHERE id = new.id
-      RETURNING id, created_at, updated_at INTO recipe_id, recipe_created_at, recipe_updated_at;
-
+    -- Validate ingredients json input
     IF new.ingredients IS NULL OR new.ingredients::text = '{}'::text THEN
       RAISE EXCEPTION 'A recipe must have ingredients!';
     END IF;
-
-    -- Delete all ingredient_groups (cascades to ingredients)
-    DELETE FROM data.ingredient_groups WHERE data.ingredient_groups.recipe_id = new.id;
-    -- Insert the new ingredient groups
-    FOR g, ingredients IN SELECT * FROM json_each(new.ingredients) LOOP
-      INSERT INTO data.ingredient_groups (name, recipe_id)
-        VALUES (g, recipe_id)
-        RETURNING id INTO ingredient_group_id;
-
+    FOR g, ingredients IN SELECT * FROM jsonb_each(new.ingredients) LOOP
         IF ingredients IS NULL OR json_array_length(ingredients) = 0 THEN
           RAISE EXCEPTION 'Ingredient group "%" must not be empty!', g;
         END IF;
-      -- Insert the ingredients in each ingredient group
-      FOR ingredient IN SELECT * FROM json_array_elements_text(ingredients) LOOP
-        INSERT INTO data.ingredients (contents, ingredient_group_id)
-        VALUES (ingredient, ingredient_group_id);
-      END LOOP;
-
     END LOOP;
+
+    -- Update the recipe
+    UPDATE data.recipes
+    SET title = new.title, description = new.description, instructions = new.instructions,
+        tags = new.tags, portions = new.portions, ingredients = new.ingredients
+    WHERE id = new.id
+      RETURNING id, created_at, updated_at INTO recipe_id, recipe_created_at, recipe_updated_at;
 
     new.id = recipe_id;
     new.created_at = recipe_created_at;
@@ -204,16 +140,30 @@ INSTEAD OF UPDATE ON api.recipes
 FOR EACH ROW EXECUTE PROCEDURE api.update_recipe();
 
 /*
- * Search
+ * Search function
+ * Builds a prefix-matching search from a normal search query
+ * Returns what the frontend calls a Recipe Preview of each matching recipe
  */
 CREATE FUNCTION api.search(search_query text)
-RETURNS SETOF api.recipes AS $$
+RETURNS 
+  table(
+    id integer,
+    title text,
+    description text,
+    created_at timestamptz,
+    updated_at timestamptz
+  ) AS $$
 WITH search AS (
   SELECT to_tsquery('swedish', string_agg(lexeme || ':*', ' & ' ORDER BY positions)) AS query
   FROM unnest(to_tsvector('swedish', search_query))
 )
--- TODO: do not simply return all columns, decide what to return or take that as a param
-SELECT api.recipes.*
+SELECT 
+  id,
+  title,
+  description,
+  created_at,
+  updated_at
 FROM api.recipes, search
 WHERE api.recipes.search @@ search.query;
 $$ LANGUAGE SQL IMMUTABLE;
+
