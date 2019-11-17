b4f971a154e8d5972c6d12996851b4fd477725f0
AxelTLarsson <mail@axellarsson.nu>
2019-11-17 18:10:36 +0100

    Fix integration tests

--- tests/rest/read.js
+++ tests/rest/read.js
@@ -7,22 +7,22 @@ describe('read', function () {
 
   it('basic', function (done) {
     rest_service()
-      .get('/todos?select=id,todo')
+      .get('/recipes?select=id,title')
       .expect('Content-Type', /json/)
       .expect(200, done)
       .expect(r => {
-        r.body.length.should.equal(3)
+        r.body.length.should.equal(2)
         r.body[0].id.should.equal(1)
       })
   })
 
   it('by primary key', function (done) {
     rest_service()
-      .get('/todos/1?select=id,todo')
+      .get('/recipes/1?select=id,title')
       .expect(200, done)
       .expect(r => {
         r.body.id.should.equal(1)
-        r.body.todo.should.equal('item_1')
+        r.body.title.should.equal('Cheese Cake')
       })
   })
 })
