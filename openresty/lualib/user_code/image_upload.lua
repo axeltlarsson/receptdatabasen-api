3813dad17bb5bf4e925f7280885fc411a85d270c
AxelTLarsson <mail@axellarsson.nu>
2019-11-24 17:17:11 +0100

    Add new endpoint /images/upload for direct binary upload
    It accepts Content-type: image/* direct binary image uploads, reads
    it in memory (does not buffer into temporary file - altough it's trivial
    to implement, but I see no reason why at this point in time), then
    calculates a random hash and returns it in the JSON response.
    
    The idea is that the client will asynchronously upload images with POST
    requests to /images/upload, which will return the new url for the saved
    image. Then the client will include this url in the POST/PATCH requests
    to the recipes endpoint, something like:
    {
      title: "Cheese Cake",
      ingredients: "...",
      header_image: { url: "/images/xyz.jpeg" },
      ...
    }
    The PostGREST endpoint /recipes will then save that url, and serve it on
    subsequent requests for the recipe.
    
    Of course, a number of issues arise:
    - the client will need to wait for the response from /images/upload
      before POST:ing the recipe
    - the clients could upload a bunch of files, then exit the session, does
      polluting the /uploads directory with a bunch of never to be used
      files. I need to write a script that cleans up this directory
      periodically.
    
    TODO:
    - [ ] mount a volume /uploads in openresty
    - [ ] endpoint to actually serve the images - with image processing
      capabilities
    - [ ] periodically clean up unused uploads (older than x hours)
    - [ ] accept images in the main recipe model (e.g. header_image)

---
+++ openresty/lualib/user_code/image_upload.lua
@@ -0,0 +1,37 @@
+local cjson = require 'cjson'
+local sha256 = require 'resty.sha256'
+local str = require 'resty.string'
+
+
+local content_type = ngx.var.content_type
+
+if not string.match(content_type, 'image/*') then
+  -- ngx.say(cjson.encode({error = 'bad content-type'}))
+  ngx.log(ngx.WARN, "Bad content-type")
+  ngx.exit(ngx.HTTP_NOT_ALLOWED)
+end
+
+
+local function file_name()
+  -- What about file extensions? Calculate from mime type?
+  local r = ngx.now() + math.random()
+  return ngx.md5(tostring(r)) .. '.jpeg'
+end
+
+-- Body is read from memory (true if client_max_body_size == client_body_buffer_size)
+ngx.req.read_body()
+local body_data =  ngx.req.get_body_data()
+
+if body_data then
+  local file_name = file_name()
+  local file_path = '/uploads/' .. file_name
+  local file = io.open(file_path, 'w+b')
+  file:write(body_data)
+  file:close()
+  local response = { image = { url = '/images/' .. file_name } }
+  ngx.say(cjson.encode(response))
+else
+  -- ngx.say(cjson.encode({error = 'Could not read the body data.'}))
+  ngx.log(ngx.WARN, "Could not read the body data.")
+  ngx.exit(ngx.HTTP_BAD_REQUEST)
+end
