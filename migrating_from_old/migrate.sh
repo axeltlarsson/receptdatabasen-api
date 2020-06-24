f4fe5815aa7ea7c8b311dfb39bdd4ebf6b8d2f55
AxelTLarsson <mail@axellarsson.nu>
2020-06-24 23:53:22 +0200

    Add proper instructions, scripts to migrate from old

---
+++ migrating_from_old/migrate.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+trap 'error_handler' ERR
+
+function error_handler {
+  echo "🚨 An error occurred, aborting script..."
+  exit 1
+}
+
+echo "Converting schema structure in 'oldrecipes' database..."
+psql -d oldrecipes -f ./migrate.sql
+
+
+echo "Dumping the converted schema to 'converted_dump.sql'..."
+pg_dump --schema=data --data-only oldrecipes > converted_dump.sql
+
+
+echo "Truncating destination table..."
+docker-compose exec db psql -U superuser -d app -c "truncate table data.recipe;"
+
+echo "Importing 'converted_dump.sql...'"
+docker-compose exec -T db psql -U superuser -d app < converted_dump.sql
+
+echo "✅ Done, please manually transfer any images."
