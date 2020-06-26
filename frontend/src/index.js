afe83027c26980dd2be20681985a3a81f08c31c9
AxelTLarsson <mail@axellarsson.nu>
2020-06-26 12:55:22 +0200

    🚨 Add eslint AirBnb config and fix linter warnings

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,74 +1,60 @@
-import "./main.css";
-import "./hourglass_loader.css";
-import "./fonts.css";
-import { Elm } from "./Main.elm";
-import * as EasyMDE from "easymde";
-import * as serviceWorker from "./serviceWorker";
+import './main.css';
+import './hourglass_loader.css';
+import './fonts.css';
+import * as EasyMDE from 'easymde';
+import { Elm } from './Main.elm';
+import * as serviceWorker from './serviceWorker';
 
 const flags = { width: window.innerWidth, height: window.innerHeight };
 const app = Elm.Main.init({
-  node: document.getElementById("root"),
-  flags: flags,
+  node: document.getElementById('root'),
+  flags,
 });
 
 class EasyMDEditor extends HTMLElement {
-  constructor() {
-    super();
-  }
-
   connectedCallback() {
-    console.log("connectedCallback", this.getAttribute("id"));
-
-    const textArea = document.createElement("textarea");
+    const textArea = document.createElement('textarea');
     this.appendChild(textArea);
-    const id = this.getAttribute("id");
+    const id = this.getAttribute('id');
 
-    let options = this.getAttribute("options");
+    let options = this.getAttribute('options');
     if (options) {
       options = JSON.parse(options);
     }
-    options = Object.assign({ toolbar: null }, options);
+    options = { toolbar: null, ...options };
 
     const easyMDE = new EasyMDE({
       element: textArea,
-      toolbar: options["toolbar"],
+      toolbar: options.toolbar,
       spellChecker: false,
-      placeholder: this.getAttribute("placeholder"),
-      initialValue: this.getAttribute("initialValue"),
+      placeholder: this.getAttribute('placeholder'),
+      initialValue: this.getAttribute('initialValue'),
     });
 
-    easyMDE.codemirror.on("change", () => {
+    easyMDE.codemirror.on('change', () => {
       const msg = {
-        type: "change",
-        id: id,
+        type: 'change',
+        id,
         value: easyMDE.value(),
       };
       app.ports.portReceiver.send(msg);
     });
 
-    easyMDE.codemirror.on("blur", () => {
+    easyMDE.codemirror.on('blur', () => {
       const msg = {
-        type: "blur",
-        id: id,
+        type: 'blur',
+        id,
       };
       app.ports.portReceiver.send(msg);
     });
   }
 
   static get observedAttributes() {
-    return ["initialValue"];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    console.log("Custom element attributes changed.");
+    return ['initialValue'];
   }
 }
 
-customElements.define("easy-mde", EasyMDEditor);
-
-app.ports.portSender.subscribe(function (elmValue) {
-  console.log("elmValue", elmValue);
-});
+customElements.define('easy-mde', EasyMDEditor);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
