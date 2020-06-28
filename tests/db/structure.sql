9d3a8848189ed04d08c5f4c8bece8b8f244ac865
AxelTLarsson <mail@axellarsson.nu>
2020-06-28 21:43:20 +0200

    Fix db tests

--- tests/db/structure.sql
+++ tests/db/structure.sql
@@ -1,21 +1,27 @@
 begin;
-select * from no_plan();
 
-select * from check_test(
-    views_are('api', array['recipes'], 'tables present' ),
-    true,
-    'all views are present in api schema',
-    'tables present',
-    ''
-);
+select
+  *
+from
+  no_plan ();
 
-select * from check_test(
-    functions_are('api', array['login', 'signup', 'refresh_token', 'me', 'search'], 'functions present' ),
-    true,
-    'all functions are present in api schema',
-    'functions present',
-    ''
-);
+select
+  *
+from
+  check_test (views_are ('api', array['recipes'], 'tables present'), true, 'all views are present
+    in api schema', 'tables present', '');
 
-select * from finish();
+select
+  *
+from
+  check_test (functions_are ('api', array['login', 'signup', 'refresh_token', 'me', 'search',
+    'insert_recipe', 'update_recipe'], 'functions present'), true, 'all functions are present in
+    api
+    schema', 'functions present', '');
+
+select
+  *
+from
+  finish ();
+
 rollback;
