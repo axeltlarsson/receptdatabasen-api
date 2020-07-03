3b5e8ee7100a64886afe6a96afe89a760e41ef4c
AxelTLarsson <mail@axellarsson.nu>
2020-07-03 23:21:49 +0200

    🔨🐞 Don't pull images on every deploy
    It's better to explictly pin versions, which will be pulled as required,
    it ensures no suprise upgrades upon deploying. It also fixes an annoying
    bug, since docker attempt to pull the "frontend_builder" service
    definition in from=frontend_builder directive in the openresty/Dockerfile,
    causing an error.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,4 @@ set -e
 # This script is called from the post-receive hook, upon deployment.
 
 echo "Deploying ${NEWREV:0:6}..."
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml build --pull
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
