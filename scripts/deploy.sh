6c284d7d82012655e387aebf66341c29aa886343
AxelTLarsson <mail@axellarsson.nu>
2020-08-15 13:16:22 +0200

    💚 Fix deploy - always build frontend before openresty
    The issues was that the frontend was build after the openresty
    container, which thus received a stale build!
    You can't specify build order in docker-compose.yml,
    neither the order of the keys nor "depends_on" (which is only for
    run-time) works.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,5 @@ set -e
 # This script is called from the post-receive hook, upon deployment.
 
 echo "Deploying ${NEWREV:0:6}..."
+(cd frontend && docker build -t receptdatabasen_frontend_builder .) 
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
