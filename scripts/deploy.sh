7632ac28d1748a9f9e7ad5b1686be7caebc3de99
AxelTLarsson <mail@axellarsson.nu>
2021-01-28 22:24:15 +0100

    🐛 Fix path to sqitch wrapper script in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,9 @@
 set -e
 
 # This script is called from the post-receive hook, upon deployment.
+# Working directory is root of the project
 
 echo "Deploying ${NEWREV:0:6}..."
 (cd frontend && docker build -t receptdatabasen_frontend_builder .) 
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
-./sqitch status --cd db/migrations
+scripts/sqitch status --cd db/migrations
