aebba8e91ba39d0d1cc8b2be5e33191353b01ca6
AxelTLarsson <mail@axellarsson.nu>
2021-01-28 22:25:29 +0100

    🐛 Add debug stmt to deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,5 @@ set -e
 echo "Deploying ${NEWREV:0:6}..."
 (cd frontend && docker build -t receptdatabasen_frontend_builder .) 
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
+echo "Running migrations..."
 scripts/sqitch status --cd db/migrations
