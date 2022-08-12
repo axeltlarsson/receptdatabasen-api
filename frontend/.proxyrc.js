75d1b138ac4c8214b45a1c0a4b048be398b68b5c
AxelTLarsson <mail@axellarsson.nu>
2022-08-13 00:33:05 +0200

    ⬆🔥 Use parcel.js to bundle the app, removing create-elm-app
    creat-elm-app is unfortunately since long unmaintained.
    parcel.js seems like a good replacement for now.
    TODO:
    - nixify further, probably node2nix

---
+++ frontend/.proxyrc.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use('/rest',
+    createProxyMiddleware( {
+      target: "http://localhost:8080/",
+    })
+  );
+  app.use('/images',
+    createProxyMiddleware( {
+      target: "http://localhost:8080/",
+    })
+  );
+};
