cc8ad17ba5ba5936170ce52191a3f9df8db67f6c
AxelTLarsson <mail@axellarsson.nu>
2023-12-29 21:40:11 +0100

    🔥 Fully remove service worker
    Better to add back later if needed.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import './hourglass_loader.css';
 import './fonts.css';
 import * as EasyMDE from 'easymde';
 import { Elm } from './Main.elm';
-// import * as serviceWorker from './serviceWorker';
 
 const flags = { width: window.innerWidth, height: window.innerHeight };
 const app = Elm.Main.init({
@@ -327,8 +326,3 @@ class EasyMDEditor extends HTMLElement {
 }
 
 customElements.define('easy-mde', EasyMDEditor);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
