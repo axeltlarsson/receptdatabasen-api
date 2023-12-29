0bbd07a0954d5ce1e66d6900ecf227b945ebf6db
AxelTLarsson <mail@axellarsson.nu>
2023-12-29 14:29:31 +0100

    🔊 Add some debug log statements

--- openresty/lua/passkeys/registration_begin.lua
+++ openresty/lua/passkeys/registration_begin.lua
@@ -2,13 +2,11 @@ local resty_session = require "resty.session"
 local utils = require "utils"
 
 -- make the request to Postgrest 
+print("lua registration_begin")
 ngx.req.read_body()
-local request_options = {}
-request_options["always_forward_body"] = true
-
 local res = ngx.location.capture(
     "/internal/rest/rpc/passkey_registration_begin",
-    request_options
+    { always_forward_body = true }
     )
 
 if res.status ~= 200 then
@@ -16,10 +14,12 @@ if res.status ~= 200 then
     ngx.status = res.status
     return ngx.say(res.body)
 else
+    print("reading challenge from body")
     -- read the challenge from the body 
     local b = cjson.decode(res.body)
     local challenge = b['challenge']
     if challenge then
+        print("the challenge was " .. challenge)
         -- and store it in session
         local session = resty_session.open()
 
@@ -29,6 +29,7 @@ else
             utils.return_error("Could not store passkey challenge in session cookie", ngx.HTTP_BAD_REQUEST)
         end
     end
+    print("responding with " .. res.body)
     ngx.status = 200
     return ngx.say(res.body)
 end
