cf6ac2fe8efb9c86d5b78788bd87752d5c365e50
axeltlarsson <mail@axellarsson.nu>
2024-03-20 21:42:36 +0100

    Revert the grant for data.passkey to api
    Role doesn't exist at that point in time, but should be done in
    privileges.sql...

--- module.nix
+++ module.nix
@@ -7,6 +7,9 @@ docker-compose-file:
 let
 
   cfg = config.services.receptdatabasen;
+  minLengthString = minLength:
+    lib.types.addCheck lib.types.str
+    (s: lib.strings.stringLength s >= minLength);
 
 in {
   options.services.receptdatabasen = {
@@ -38,6 +41,49 @@ in {
         This only has an effect if the domain option is set
       '';
     };
+
+    superuserPassword = lib.mkOption {
+      type = minLengthString 8;
+      default = "superpassword";
+      description = ''
+        The password for the database `superuser` role
+
+        Tip: use `< /dev/urandom tr -dc 'A-Za-z0-9' | head -c 32` to generate a random secret
+      '';
+    };
+
+    dbPassword = lib.mkOption {
+      type = minLengthString 8;
+      default = "password";
+      description = ''
+        The password for the database user `authenticator`
+
+        Tip: use `< /dev/urandom tr -dc 'A-Za-z0-9' | head -c 32` to generate a random secret
+      '';
+    };
+
+    jwtSecret = lib.mkOption {
+      type = minLengthString 32;
+      example = "b6tWWsskVX6Id7LbqKWy6eHAiVumExzR";
+      description = ''
+        The secret to use for JWT tokens
+        Must be at least 32 characters long!
+
+        Tip: use `< /dev/urandom tr -dc 'A-Za-z0-9' | head -c 32` to generate a random secret
+      '';
+    };
+
+    cookieSessionSecret = lib.mkOption {
+      type = minLengthString 32;
+      example = "fQGUNzfLgA6l5wMazvRcDJ2IuauIMSiR";
+      description = ''
+        The secret to use for cookie sessions
+        Must be at least 32 characters long!
+
+        Tip: use `< /dev/urandom tr -dc 'A-Za-z0-9' | head -c 32` to generate a random secret
+      '';
+    };
+
   };
 
   config = lib.mkIf cfg.enable {
@@ -61,13 +107,13 @@ in {
         COMPOSE_PROJECT_NAME = "receptdatabasen";
 
         SUPER_USER = "superuser";
-        SUPER_USER_PASSWORD = "superpassword";
-        DB_PASS = "passwordpassswordpassword";
-        JWT_SECRET = "AG7Yo/BbqGZUm75BZ5HfDkexDKAIumSCJY+eoyh9f1sr"; # need to be minimum 32 characters
+        SUPER_USER_PASSWORD = cfg.superuserPassword;
+        DB_PASS = cfg.dbPassword;
+        JWT_SECRET = cfg.jwtSecret;
         RP_ID = "'${rp_id}'";
         ORIGIN = "'${origin}'";
 
-        COOKIE_SESSION_SECRET = "TFp3nhFY_mUFbHqBoSkYnIbhBBKVr1ar";
+        COOKIE_SESSION_SECRET = cfg.cookieSessionSecret;
         OPENRESTY_PORT = toString cfg.port;
       };
       serviceConfig = {
