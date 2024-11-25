6373df4593fd943e64d67c5abf4a70ced7269152
axeltlarsson <mail@axellarsson.nu>
2024-11-25 11:42:49 +0100

    🛂 Authenticate against listan with jwt
    - added lua-resty-jwt
    - added lua-resty-http

--- openresty/lua/shoppinglist_export.lua
+++ openresty/lua/shoppinglist_export.lua
@@ -1,5 +1,6 @@
 local utils = require "utils"
-local resty_session = require "resty.session"
+local cjson = require "cjson"
+local jwt = require "resty.jwt"
 
 -- Extract the recipe ID using ngx.re.match
 local recipe_id_match = ngx.re.match(ngx.var.uri, "^/export_to_list/(\\w+)$")
@@ -18,21 +19,54 @@ if recipe.status ~= 200 then
 end
 
 
--- This should only ever be called with a valid session already in place, let's fetch the session
-local session, err, exists, refreshed = resty_session.start()
-if not exists or not session:get("jwt") then
-    -- Shoul never happen since we require a valid session to access this endpoint
-    utils.return_error("You need a valid session to access this endpoint", ngx.HTTP_UNAUTHORIZED)
+-- get the user information from /me
+local user = ngx.location.capture(
+    "/internal/rest/rpc/me",
+    { method = ngx.HTTP_GET }
+)
+
+-- if we for some reason can't find the user, return the error
+if user.status ~= 200 then
+    utils.return_error("Could not find user", user.status)
 end
 
--- debug print the session
-print(session:get("jwt"))
+-- extract the user ID from the response
+local user_name = cjson.decode(user.body).user_name
+print("Adding ingredients to list for user: " .. user_name)
+
+-- prepare the payload for the http request
+-- we need to create a jwt token to authenticate the request
+local function generate_jwt(listan_user_name)
+    local secret = os.getenv("LISTAN_JWT_SECRET") -- Shared secret between listan and receptdatabasen
+    local token = jwt:sign(
+        secret,
+        {
+            header = { typ = "JWT", alg = "HS256" },
+            payload = {
+                iss = "receptdatabasen",
+                aud = "listan",
+                sub = listan_user_name,
+                scope = "add:ingredients",
+                iat = ngx.now(),
+                exp = ngx.now() + 600 -- Expires in 10 minutes
+            }
+        }
+    )
+    return token
+end
+
+local jwt_token = generate_jwt(user_name) -- assume same user_name in both services
+
 
 -- make an HTTP request to the listan API in order to add the ingredients to the list
--- how to know which list to add the ingredients to?
--- how to know which user is making the request?
--- given that we would know the user from the session, we could use the user's ID to fetch the list ID
+-- how to know which list to add the ingredients to? - we just assume the first available list for now
+-- how to know which user is making the request? - we know from a call to /me to get the user_name
+-- make the external http request to the listan API
+
+local api_url = os.getenv("LISTAN_API_URL") .. "/lists/batch/"
+print("POST to " .. api_url)
+
 
 -- Return the response from PostgREST as is for now
 ngx.status = recipe.status
-ngx.say(recipe.body)
+ngx.say(cjson.encode({ jwt = jwt_token, recipe = cjson.decode(recipe.body) }))
