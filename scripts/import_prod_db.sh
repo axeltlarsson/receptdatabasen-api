b14131ca566e808cf125f6b1202dc1de92862470
axeltlarsson <mail@axellarsson.nu>
2025-04-15 09:17:44 +0200

    🔨 Fix db scripts

--- scripts/import_prod_db.sh
+++ scripts/import_prod_db.sh
@@ -16,6 +16,7 @@ Available options:
 -h, --help      Print this help and exit
 -v, --verbose   Print script debug info
 -d, --download  Also download images 
+--docker				Download images to docker container (otherwise uses on-metal Nix)
 EOF
 	exit
 }
@@ -45,34 +46,36 @@ die() {
 
 parse_params() {
 	# default values of variables set from params
-	download=0
+	DOWNLOAD=0
+	DOCKER=0
 
-	while :; do
+	while [[ $# -gt 0 ]]; do
 		case "${1-}" in
 		-h | --help) usage ;;
 		-v | --verbose) set -x ;;
 		--no-color) NO_COLOR=1 ;;
-		-d | --download) download=true ;; # download images too
+		-d | --download) DOWNLOAD=1 ;; # download images too
+		--docker) DOCKER=1 ;;          # download images to docker container (otherwise uses on-metal Nix)
 		-?*) die "Unknown option: $1" ;;
-		*) break ;;
+		*)
+			# Collect all non-option arguments
+			args+=("$1")
+			;;
 		esac
 		shift
 	done
 
-	args=("$@")
-
 	# check required params and arguments
-	# [[ -z "${param-}" ]] && die "Missing required parameter: param"
 	[[ ${#args[@]} -eq 0 ]] && usage && die "Missing script arguments"
 
 	return 0
 }
 
+# Initialize the args array
+args=()
 parse_params "$@"
 host=${args[0]}
 setup_colors
-
-# script logic here
 
 msg "${YELLOW}Import production database dump...${NOFORMAT}"
 ssh "$1" 'cd /srv/receptdatabasen && docker compose -f docker-compose.yml -f docker-compose.prod.yml exec -T db pg_dump -U superuser -d app' >prod-dump.sql 2>/dev/null
@@ -85,17 +88,42 @@ msg "${YELLOW}Import production into local db...${NOFORMAT}"
 psql -h localhost -U "$SUPER_USER" -d app <prod-dump.sql
 rm prod-dump.sql
 
-if [ -n "$download" ]; then
+# create test users and set some settings for local usage
+msg "${YELLOW}Configuring prod db for local usage...${NOFORMAT}"
+psql -h localhost -U "$SUPER_USER" -d app <<EOF
+	begin;
+	\echo Creating test users;
+	insert into data.user (user_name, password) values ('alice', 'pass');
+	insert into data.user (user_name, password) values ('bob', 'pass');
+
+	\echo Setting up settings to match default .env;
+	select settings.set('origin', 'http://localhost:1234');
+	select settings.set('rp_id', 'localhost');
+	select settings.set('image_server_secret', '5b1c7df3d10cfa988b2830562662d45c');
+	select settings.set('jwt_secret', 'AG7Yo/BbqGZUm75BZ5HfDkexDKAIumSCJY+eoyh9f1sr');
+	select settings.set('disable_user_verification', 'true');
+	commit;
+EOF
+
+if [ "$DOWNLOAD" -eq 1 ]; then
 	msg "${YELLOW}Downloading images..${NOFORMAT}"
 	ssh "$host" "mkdir -p /tmp/uploads-recept"
 	ssh "$host" "docker run -i --rm -v receptdatabasen_uploads-vol:/uploads -v /tmp/uploads-recept:/target ubuntu tar cvf /target/backup.tar /uploads > /dev/null"
 	scp "$host":/tmp/uploads-recept/backup.tar .
 	ssh "$host" "rm -rf /tmp/uploads-recept"
-	docker compose exec openresty bash -c "rm -rf /uploads/*"
-	docker cp backup.tar receptdatabasen_openresty_1:/uploads
-	docker compose exec openresty bash -c "cd /uploads && tar -xf backup.tar && mv uploads/* . && rmdir uploads && rm backup.tar"
-	docker compose exec openresty bash -c "chown -R nobody /uploads/*"
-	rm backup.tar
+
+	if [ "$DOCKER" -eq 1 ]; then
+		docker compose exec openresty bash -c "rm -rf /uploads/*"
+		docker cp backup.tar receptdatabasen_openresty_1:/uploads
+		docker compose exec openresty bash -c "cd /uploads && tar -xf backup.tar && mv uploads/* . && rmdir uploads && rm backup.tar"
+		docker compose exec openresty bash -c "chown -R nobody /uploads/*"
+		rm backup.tar
+	else
+		echo "mkdir -p $FILE_UPLOAD_PATH"
+		mkdir -p "$FILE_UPLOAD_PATH"
+		mv backup.tar "$FILE_UPLOAD_PATH"/backup.tar
+		cd "$FILE_UPLOAD_PATH" && tar -xf backup.tar && mv uploads/* . && rmdir uploads && rm backup.tar
+	fi
 fi
 
 msg "✅"
